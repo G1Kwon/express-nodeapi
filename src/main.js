da27f582843bb2ccc3b2a47e014502423da3128b
g1.kwon <spring_shot@naver.com>
2021-11-22 05:01:29 +0900

    미들웨어 개념 이해하고 만들기

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // @ts-check
 
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 const PORT = 4000
 
-app.use('/', (req, res) => {
-    res.send('Hello, express!')
+app.use('/', async (req, res, next) => {
+    console.log('Middleware 1-1')
+
+    const requestedAt = new Date()
+    const fileContent = await fs.promises.readFile('.gitignore')
+    // @ts-ignore
+    req.requestedAt = requestedAt
+    // @ts-ignore
+    req.fileContent = fileContent
+    setTimeout(() => {
+        next()
+    }, 1000)
+    },
+    (req, res, next) => {
+      console.log('Middleware 1-2')
+      next()
+    })
+
+app.use((req, res) => {
+    console.log('Middleware 2')
+    res.send(`Hello, express!: Requested at ${req.requestedAt}, ${req.fileContent}`)
 })
 
 app.listen(PORT, () => {
